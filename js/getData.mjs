f98a75bc46d1adfb3021142fa61c2e5aa0bc1aeb
HaniaFlores <hanniaflores03@gmail.com>
2023-07-02 08:07:02 -0600

    getData

---
+++ js/getData.mjs
@@ -0,0 +1,31 @@
+async function convertToJson(res) {
+  const data = await res.json();
+  if (res.ok) {
+    return data;
+  } else {
+    throw { name: "servicesError", message: data };
+  }
+}
+
+export async function getRecipeList(keyword) {    
+    const url = 'https://api.api-ninjas.com/v1/recipe?query=' + keyword;
+    const apiKey = 'd89baii574+c3Cmel9YXPg==lOPH7N4ylzgy1WoJ';
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-Api-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await convertToJson(response);
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
---
+++ js/recipe.js
@@ -0,0 +1,5 @@
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
+
+/* CALL RECIPE CONTENT FUNTION */
