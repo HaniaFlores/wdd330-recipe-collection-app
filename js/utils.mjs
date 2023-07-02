8fb34c7a8c77874a62f8ff82324ce3c9e33586e0
HaniaFlores <hanniaflores03@gmail.com>
2023-07-01 20:52:13 -0600

    header

--- js/utils.mjs
+++ js/utils.mjs
@@ -36,3 +36,23 @@ export async function loadHeaderFooter() {
   renderWithTemplate(headerTemplateFn, headerEl); 
   renderWithTemplate(footerTemplateFn, footerEl);
 }
+
+/* export function breadcrumbs() {
+  // Obtener los elementos del DOM
+  var searchButton = document.getElementById("search__button");
+  var searchBar = document.getElementById("search__bar");
+
+  // Agregar un evento click al botón de búsqueda
+  searchButton.addEventListener("click", function() {
+    // Obtener el valor del input
+    var searchQuery = searchBar.value;
+    console.log(searchQuery);
+
+    // Almacenar el valor en una variable o hacer lo que desees con él
+    // Ejemplo:
+    localStorage.setItem("searchQuery", searchQuery);
+
+    // Redirigir a otra página
+    window.location.href = "./recipe-list/index.html";
+  });
+} */
